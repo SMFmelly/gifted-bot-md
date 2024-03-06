13b70996465c7f474fdb07fa51b41efb9582ebaa
Young Genius <138666345+SMFmelly@users.noreply.github.com>
2024-03-07 02:01:40 +0300

    Update config.js
    Signed-off-by: Young Genius <138666345+SMFmelly@users.noreply.github.com>

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
 global.read_status = process.env.AUTO_READ_STATUS || "true"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
 global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254728782591,923xxxxxxxx";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254790741059,923xxxxxxxx";
 
 global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://qr-scan.giftedtechnexus.co.ke/";
